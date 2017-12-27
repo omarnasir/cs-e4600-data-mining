3fa8965aad8bf5d5d8354e51c7a87178c369ec8b
omarnasir <momar.nasir@gmail.com>
2017-12-28 03:07:46 +0500

    changed task 1 to use igraph functions

--- task_1_igraph.py
+++ task_1_igraph.py
@@ -1,5 +1,4 @@
-from networkx import all_pairs_shortest_path_length, shortest_path_length, single_source_shortest_path_length, eccentricity, diameter
-from networkx import write_gpickle, read_gpickle
+from igraph import *
 import numpy as np
 import time
 import multiprocessing as mp
@@ -9,62 +8,61 @@ class task_1:
     graph = None
     
     def getdistance(self, node):
-        return single_source_shortest_path_length(self.graph, node)
+        return self.graph.shortest_paths(source=node)[0]
 
     def getdistancesumcount(self, node):
         sum_tmp = 0
         count = 0
-        distance_node = single_source_shortest_path_length(self.graph, node)
-        distance = np.array(list(distance_node.values()))
+        distance_node = self.graph.shortest_paths(source=node)[0]
+        distance = np.array(distance_node)
         sum_tmp += sum(distance)
         count += len(distance)
-        # print(sum_tmp)
         return [sum_tmp, count]
 
+    def getshortestpaths(self, node, index):
+        min_paths = self.graph.shortest_paths(source=node)[0]
+        del min_paths[index]
+        return max(np.array(min_paths))
 
     def median(self):
         pool = mp.Pool(processes=5)
-        results = pool.map(self.getdistance, self.graph.nodes())
-        output = [list(res.values()) for res in results]
+        results = pool.map(self.getdistance, self.graph.vs.indices)
+        output = [res for res in results]
         pool.close()
         pool.join()
-        # distance = []
-        # for node in graph.nodes():
-        #     distance_node = single_source_shortest_path_length(graph, node)
-        #     distance.extend(np.array(list(distance_node.values())))
         return np.median(output)
 
     def mean(self):
         pool = mp.Pool(processes=5)
-        results = pool.map(self.getdistancesumcount, self.graph.nodes())
+        results = pool.map(self.getdistancesumcount, self.graph.vs.indices)
         output = [res for res in results]
         pool.close()
         pool.join()
         sum_all= np.sum(output, axis=0)
-        print(sum_all[0])
         d_mean = sum_all[0] / sum_all[1]
-        # distance = []
-        # for node in self.graph.nodes():
-        #     distance_node = single_source_shortest_path_length(self.graph, node)
-        #     distance.extend(np.array(list(distance_node.values())))
         return d_mean
 
     def diameter(self):
         d = 0
+        pool = mp.Pool(processes=5)
         min_paths = []
-        for node in self.graph.nodes():
-            min_paths = shortest_path_length(self.graph, node)
-            del min_paths[node]
-            min_paths = np.array(list(min_paths.values()))
-            d_node = max(min_paths) 
-            if d_node > d:
-                d = d_node
-        return d
+        results = pool.map(self.getdistancesumcount, self.graph.vs.indices, range(0,len(self.graph.vs.indices)))
+        output = [res for res in results]
+        pool.close()
+        pool.join()
+        
+        # for index,node in enumerate(self.graph.vs.indices):
+        #     min_paths = self.graph.shortest_paths(source=node)[0]
+        #     del min_paths[index]
+        #     d_node = max(np.array(min_paths))
+        #     if d_node > d:
+        #         d = d_node
+        return min(np.array(output))
 
     def effective_diameter(self):
         eff_d = []
-        for node in self.graph.nodes():
-            d_node = eccentricity(self.graph, node)
+        for node in self.graph.vs.indices:
+            d_node = self.graph.eccentricity(vertices=node)
             eff_d.append(d_node)
         eff_d_sorted = np.sort(eff_d)
         return eff_d_sorted[int(np.floor(len(eff_d_sorted)*0.9))]
@@ -81,13 +79,14 @@ class task_1:
             print('Value: ' + str(value))
             print('Elapsed Time: %.3f seconds.' % elapsed)
     
-# def main():
-#     graph = read_gpickle('D:/Project/outputs/wiki-Vote_LSCC.txt')
-#     start = time.perf_counter()
-#     value = diameter(graph)
-#     elapsed = time.perf_counter() - start
-#     print('Value: ' + str(value))
-#     print('Elapsed Time: %.3f seconds.' % elapsed)
+def main():
+    obj = task_1()
+    obj.graph = Graph.Read_GraphMLz('D:/Project/outputs_igraph/wiki-Vote_LWCC.txt')
+    start = time.perf_counter()
+    value = obj.diameter()
+    elapsed = time.perf_counter() - start
+    print('Value: ' + str(value))
+    print('Elapsed Time: %.3f seconds.' % elapsed)
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
