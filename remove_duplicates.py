9e667f05d3f82f17ff2d2091d0ee65acadfc2d0b
lauradlz <laura.dlzmrtnz@gmail.com>
2017-12-27 22:03:11 +0100

    change names of file

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def main():
-    data = pd.read_csv('ego-Gplus-original.txt', sep=" ", header=None)
+    data = pd.read_csv('gplus_combined.txt', sep=" ", header=None)
     # data = pd.read_csv('new.txt', sep=" ", header=None)
     data = data.drop_duplicates(keep='first')
     data.to_csv('ego-Gplus.txt', sep=" ", header=None, index=False)
