333902065456e563ef3ed88a1c4e96bad3ed8263
omarnasir <momar.nasir@gmail.com>
2017-12-28 01:35:34 +0500

    added remove_duplicates code. fixed subgraph computing

---
+++ remove_duplicates.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def main():
+    data = pd.read_csv('ego-Gplus.txt', sep=" ", header=None)
+    # data = pd.read_csv('new.txt', sep=" ", header=None)
+    data = data.drop_duplicates(keep='first')
+    data.to_csv('ego-Gplus-modified.txt', sep=" ", header=None, index=False)
+
+if __name__ == "__main__":
+    main()
--- task_0_igraph.py
+++ task_0_igraph.py
@@ -2,33 +2,31 @@ from igraph import *
 
 class task_0:
     graph = None
-    SCC_largest = None
-
-    def read_graph(self, filename, isdirected):
-        print("------------------Reading Graph From Disk--------------------")
-        self.graph = Graph.Read_Ncol(filename, directed=isdirected)  
-        print("------------------Reading Completed-----------------")
+    scc_largest = None
     
     def task_0(self, directed):
         print("------------------Computing SCCs--------------------")
         if(directed):
-            self.SCC_largest = max(self.graph.components(mode="STRONG"), key=len)            
+            self.scc_largest = max(self.graph.components(mode="STRONG"), key=len)            
         else:
-            self.SCC_largest = max(self.graph.components(mode="WEAK"), key=len)
-        self.SCC_largest = self.graph.subgraph(self.SCC_largest)
+            self.scc_largest = max(self.graph.components(mode="WEAK"), key=len)
+        # Extract the subgraph for the largest connected component
+        self.scc_largest = self.graph.subgraph(self.scc_largest)
         print("------------------Completed-----------------")
-        return self.SCC_largest.summary()
+        return self.scc_largest.summary()
 
     def lscc_lwcc_generator(self):
-        directed = False
-        filenames = ['soc-Pokec']
-        # filenames = ['wiki-Vote', 'soc-Epinions1', 'soc-Pokec', 'ego-Gplus']
+        directed = False # Set to True for directed graphs
+        filenames = ['wiki-Vote', 'soc-Epinions1', 'soc-Pokec', 'ego-Gplus']
+        
         for file_obj in filenames:
+            print("------------------Reading Graph From Disk--------------------")
             ### Local computing
-            self.read_graph('D:/Project/data/' + file_obj + '.txt', directed)
-            # self.read_graph('C:/Users/Pc Laura/Desktop/Data_Mining/Project/cs-e4600-data-mining/data/' + file_obj + '.txt', directed
+            self.graph = Graph.Read_Ncol('D:/Project/data/' + file_obj + '.txt', directed=directed) 
+            # self.graph = Graph.Read_Ncol('C:/Users/Pc Laura/Desktop/Data_Mining/Project/cs-e4600-data-mining/data/' + file_obj + '.txt',directed=directed)
             ### For Aalto Notebooks
-            # self.read_graph('./data/' + file_obj + '.txt', directed)
+            # self.graph = Graph.Read_Ncol('./data/' + file_obj + '.txt', directed=directed)
+            print("------------------Reading Completed-----------------")
             info = self.task_0(directed)
             if(directed):
                 ext = '_LSCC.txt'
@@ -39,10 +37,10 @@ class task_0:
             print("Edges/Vertices: " + info)
             print("------------------Writing LCCs to Disk--------------------")
             ### Local computing
-            self.SCC_largest.write_graphmlz(f='D:/Project/outputs_igraph/'+ file_obj + ext)
-            # self.SCC_largest.write_graphmlz(f='C:/Users/Pc Laura/Desktop/Data_Mining/Project/cs-e4600-data-mining/outputs/'+ file_obj + ext)
+            self.scc_largest.write_graphmlz(f='D:/Project/outputs_igraph/'+ file_obj + ext)
+            # self.scc_largest.write_graphmlz(f='C:/Users/Pc Laura/Desktop/Data_Mining/Project/cs-e4600-data-mining/outputs/'+ file_obj + ext)
             ### For Aalto Notebooks
-            # self.SCC_largest.write_graphmlz(f='./outputs/'+ file_obj + ext)
+            # self.scc_largest.write_graphmlz(f='./outputs/'+ file_obj + ext)
             print("------------------Completed-----------------")
             print("Iteration finished")
 
